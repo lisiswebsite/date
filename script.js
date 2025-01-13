2997a29a921b1e51577ff4d4168870ae791e2cf1
lisiswebsite <lisiswebsite@gmail.com>
2025-01-12 20:33:28 -0500

    added frame

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const textElement = document.getElementById("intro-text");
     const optionsElement = document.querySelector(".options");
+    const player1Element = document.getElementById("player1");
     const text = textElement.innerHTML;
     textElement.innerHTML = "";
     let index = 0;
@@ -9,9 +10,10 @@ document.addEventListener("DOMContentLoaded", function() {
         if (index < text.length) {
             textElement.innerHTML += text.charAt(index);
             index++;
-            setTimeout(typeWriter, 18); // Adjust typing speed here
+            setTimeout(typeWriter, 17); // Adjust typing speed here
         } else {
             optionsElement.classList.add("visible"); // Show options with fade-in effect
+            player1Element.classList.add("visible"); // Slide in the image
         }
     }
 
