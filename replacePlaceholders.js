e38491a0ee7b3334950a3d604543de35e692b3b4
lisiswebsite <lisiswebsite@gmail.com>
2025-02-01 02:45:48 -0500

    changed players to roster players

--- replacePlaceholders.js
+++ replacePlaceholders.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const player1No = 1; // Specify the player number for Player1
-    const player2No = 38; // Specify the player number for Player2
+    const player1No = 33; // Specify the player number for Player1
+    const player2No = 32; // Specify the player number for Player2
     const player3No = 57; // Specify the player number for Player3
 
     const player1 = players.find(player => player.no === player1No);
